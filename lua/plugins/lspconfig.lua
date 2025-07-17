0c9d86c0e47394ad7c02b1e3760a1929d282906f
Aswathaman-Ramalingam <aswathaman.official@gmail.com>
2025-07-17 16:02:15 +0530

    lsp-buffer error fixed

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,41 +1,46 @@
 return {
-  {
-    'mason-org/mason-lspconfig.nvim',
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-    dependencies = {
-      'mason-org/mason.nvim',
-      'neovim/nvim-lspconfig',
-    },
-    config = function()
-      require('mason').setup()
-      require('mason-lspconfig').setup {
-        ensure_installed = {
-          'htmx',
-          'jdtls',
-          'lua_ls',
-          'pyright',
-          'rust_analyzer',
-          'svelte',
-          'tailwindcss',
-          'ts_ls',
-          -- 'gopls',
+    {
+        'mason-org/mason-lspconfig.nvim',
+        lazy = false,
+        opts = {
+            auto_install = true,
         },
-      }
+        dependencies = {
+            'mason-org/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
+        config = function()
+            require('mason').setup()
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    'htmx',
+                    'jdtls',
+                    'lua_ls',
+                    'pyright',
+                    'rust_analyzer',
+                    'svelte',
+                    'tailwindcss',
+                    'ts_ls',
+                    -- 'gopls',
+                },
+            }
 
-      local bufopts = { buffer = bufnr }
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-      vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, bufopts)
-    end,
-  },
-  {
-    'mason-org/mason.nvim',
-    opts = {},
-  },
-  {
-    'neovim/nvim-lspconfig',
-  },
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufopts = { buffer = args.buf }
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                    vim.keymap.set('n', 'gra', vim.lsp.buf.code_action, bufopts)
+                end,
+            })
+        end,
+    },
+    {
+        'mason-org/mason.nvim',
+        opts = {},
+    },
+    {
+        'neovim/nvim-lspconfig',
+    },
 }
