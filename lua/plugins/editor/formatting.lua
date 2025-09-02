f290bd4456339f75acdaf76af7b62c9d92d5c80b
Aswathaman-Ramalingam <aswathaman.official@gmail.com>
2025-09-02 21:44:15 +0530

    formatting added to files like go, json, .md, yaml and etc

--- lua/plugins/editor/formatting.lua
+++ lua/plugins/editor/formatting.lua
@@ -1,68 +1,72 @@
 -- Formatting & tags
 
 return {
-    {
-        'stevearc/conform.nvim',
-        event = { 'BufWritePre' },
-        cmd = { 'ConformInfo' },
-        keys = {
-            {
-                '<leader>f',
-                function()
-                    require('conform').format { async = true, lsp_format = 'fallback' }
-                end,
-                mode = '',
-                desc = '[F]ormat buffer',
-            },
-        },
-        opts = {
-            notify_on_error = false,
-            format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
-                if disable_filetypes[vim.bo[bufnr].filetype] then
-                    return nil
-                else
-                    return {
-                        timeout_ms = 500,
-                        lsp_format = 'fallback',
-                    }
-                end
-            end,
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                python = { 'black' },
-                javascript = { 'prettierd', 'prettier', stop_after_first = true },
-                javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-                typescript = { 'prettierd', 'prettier', stop_after_first = true },
-                typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
-                svelte = { 'prettierd' },
-                html = { 'prettierd' },
-                rust = { 'rustfmt' },
-                java = { 'google-java-format' },
-            },
-        },
-    },
-    {
-        'windwp/nvim-ts-autotag',
-        config = function()
-            require('nvim-ts-autotag').setup({
-                opts = {
-                    enable = true,
-                    enable_close = true,
-                    enable_rename = true,
-                    enable_close_on_slash = true,
-                    filetypes = {
-                        'html',
-                        'javascript',
-                        'javascriptreact',
-                        'typescript',
-                        'typescriptreact',
-                        'svelte',
-                    },
-                },
-            })
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
-        event = 'InsertEnter',
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
     },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'black' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        svelte = { 'prettierd' },
+        html = { 'prettierd' },
+        rust = { 'rustfmt' },
+        java = { 'google-java-format' },
+        go = { 'gofmt' },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        yaml = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+    },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable = true,
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = true,
+          filetypes = {
+            'html',
+            'javascript',
+            'javascriptreact',
+            'typescript',
+            'typescriptreact',
+            'svelte',
+          },
+        },
+      }
+    end,
+    event = 'InsertEnter',
+  },
 }
-
